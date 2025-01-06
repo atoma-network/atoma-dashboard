0551a5a119c65ee7dd39d6ecbf0ac4c0d96070b5
francis2tm <franciscomelot@hotmail.com>
2025-01-06 00:32:39 +0000

    deal with basepath

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-// const basePath = process.env.NODE_ENV === 'production' ? '/atoma-dashboard' : '';
-
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/atoma-dashboard',
+  assetPrefix: '/atoma-dashboard/',
   images: {
     unoptimized: true,
   },
-  // basePath: basePath,
-  // assetPrefix: basePath,
   eslint: {
     ignoreDuringBuilds: true,
   }
