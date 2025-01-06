9b67d9f6d056b003905fcc8388e86fb70046d75c
francis2tm <franciscomelot@hotmail.com>
2025-01-06 00:27:51 +0000

    Refactor Next.js configuration: remove legacy next.config.js, update next.config.ts with new settings and commented-out basePath and assetPrefix options.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+// const basePath = process.env.NODE_ENV === 'production' ? '/atoma-dashboard' : '';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // basePath: basePath,
+  // assetPrefix: basePath,
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
