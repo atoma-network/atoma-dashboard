4d9d3ea8374dddfb28492a3f80f54925bce83240
Francisco Melo <43780565+francis2tm@users.noreply.github.com>
2025-01-07 16:13:01 +0000

    feat: GitHub pages deployment (#5)
    * Add Next.js configuration, update package.json for deployment, and set up GitHub Actions workflow for GitHub Pages
    
    * Add Next.js configuration, update package.json for deployment, and set up GitHub Actions workflow for GitHub Pages
    
    * Remove outdated GitHub Actions workflow for Atoma Proxy dashboard and update deployment branch name in GitHub Pages workflow.
    
    * Remove outdated GitHub Actions workflow for Atoma Proxy dashboard and update deployment branch name in GitHub Pages workflow.
    
    * Delete CNAME
    
    * Update proxy URL and USDC type in atoma.ts; replace environment variables with hardcoded values for testing
    
    * Refactor Next.js configuration: remove legacy next.config.js, update next.config.ts with new settings and commented-out basePath and assetPrefix options.
    
    * deal with basepath
    
    * Update GitHub Actions workflow to deploy from 'main' branch instead of 'feat/github_pages-deployment'
    
    * Update GitHub Actions workflow to deploy from 'with-logic' branch instead of 'main'
    
    * Refactor atoma.ts to use environment variables for proxy URL and USDC type; add error handling for missing proxy wallet address
    
    * Update GitHub Actions workflow to include environment variables for proxy URL, USDC type, and proxy wallet in the build step
    
    * Remove unnecessary blank line in atoma.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/atoma-dashboard',
+  assetPrefix: '/atoma-dashboard/',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
