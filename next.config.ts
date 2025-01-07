083e58bb924fda805eff98552182ea0692e936fd
francis2tm <franciscomelot@hotmail.com>
2025-01-07 16:30:00 +0000

    Refactor Next.js configuration and update GitHub Actions deployment branch
    - Comment out basePath and assetPrefix in next.config.ts for flexibility.
    - Change deployment branch in GitHub Actions workflow from 'with-logic' to 'fix/deployment' for clarity.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/atoma-dashboard',
-  assetPrefix: '/atoma-dashboard/',
+  // basePath: '/atoma-dashboard',
+  // assetPrefix: '/atoma-dashboard/',
   images: {
     unoptimized: true,
   },
