7d9f4419d5f920d782bc3b6fb71aa3f893ddb36e
francis2tm <franciscomelot@hotmail.com>
2025-01-07 18:28:33 +0000

    chore: added comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import "@/styles/globals.css"
-import { Inter } from "next/font/google"
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
+// Root layout component for the Atoma Dashboard
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
